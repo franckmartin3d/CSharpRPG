c9cba70f68508a5f8a09d47f31dc6cfae4b19c7f
franckmartin3d <franck.martin3d@gmail.com>
2019-02-26 08:04:42 +1100

    Player class

---
+++ SOSCSRPG/Engine/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    class Player
+    {
+        string Name { get; set; }
+        string CharacterClass { get; set; }
+        int HitPoints { get; set; }
+        int ExperienscePoints { get; set; }
+        int Level { get; set; }
+        int Gold { get; set; }
+    }
+}
